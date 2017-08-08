7a6571b88e4f778b6dda7cc41b5a66eba5e77f2c
Sedim Al-Araji <sedim@launchit.com>
2017-08-07 23:00:36 -0400

    Added the delete method

--- server/server.js
+++ server/server.js
@@ -64,7 +64,7 @@ app.get('/todos', (req, res) => {
 //******* End get handling
 
 
-// to handle the client side request: GET /todos/12345
+//********** to handle the client side request: GET /todos/12345
 //var id = "598783340a509ae7a16fe433";
 
 app.get('/todos/:id', (req, res) =>{
@@ -85,6 +85,30 @@ app.get('/todos/:id', (req, res) =>{
   });
     //this is a test
 });
+
+//********** to handle the remove
+app.delete('/todos/:id', (req, res) =>{
+  var id = req.params.id //this gets the sent id
+
+  if (!ObjectID.isValid(id)) {// if id is not valid
+    return res.status(400).send('***ID Not valid');
+  }
+
+  Todo.findByIdAndRemove(id).then ((todoResult) => {
+
+    if (!todoResult){
+      return res.status(404).send({});// If id not found
+    }
+    res.send({todoResult});// id found send it back as an object
+  }).catch((e) => {
+    res.status(500).send('***Database or server error');
+  });
+    //this is a test
+});
+
+
+
+
 
 
 
